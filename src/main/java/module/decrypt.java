b3bf80f8cccb2402da7daea8803e6a7ddbc1604d
Jeremic Arose <arosejeremic5@gmail.com>
2019-07-22 11:47:16 +0300

    add readme

--- src/main/java/module/decrypt.java
+++ src/main/java/module/decrypt.java
@@ -4,15 +4,30 @@ public class decrypt {
     private String word;
     private int key;
 
+    public String getWord(){
+        return word;
+    }
+
+
+    public int getKey(){
+        return key;
+    }
+
     public decrypt(String word,int key){
         this.word = word;
         this.key = key;
     }
-    public String getWord(){
-        return word;
+
+    public String decrypter(){
+        String decryptedText = "";
+        for (int i = 0; i < getWord().length(); i++){
+            char wordChar = getWord().charAt(i);
+            char decrypted = (char) (wordChar - getKey());
+            decryptedText += decrypted;
+        }
+        return decryptedText;
     }
-    public int getKey(){
-        return key;
-    }
+
 }
+
 
--- src/main/java/module/encrypt.java
+++ src/main/java/module/encrypt.java
@@ -4,14 +4,28 @@ public class encrypt {
     private String word;
     private int key;
 
-    public encrypt(String word,int key){
-        this.word = word;
-        this.key = key;
-    }
     public String getWord(){
         return word;
     }
     public int getKey(){
         return key;
     }
+
+    public encrypt(String word,int key){
+        this.word = word;
+        this.key = key;
+    }
+
+    public String encrypter(){
+        StringBuilder encryptedText = new StringBuilder();
+
+        for(int i = 0; i < getWord().length(); i++ ){
+            char wordChar = getWord().charAt(i);
+            char encrypted =  (char) (wordChar + getKey());
+            encryptedText.append(encrypted);
+        }
+        return encryptedText.toString();
+
+    }
+
 }
